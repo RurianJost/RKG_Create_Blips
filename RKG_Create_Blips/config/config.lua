04b3c10741cee1ffb0edff8d765517728d7ef550
RurianJost <rurijost@gmail.com>
2022-07-15 13:10:07 -0300

    Updated comments on config.lua

--- RKG_Create_Blips/config/config.lua
+++ RKG_Create_Blips/config/config.lua
@@ -2,11 +2,11 @@ Config_Blips = {
     Command = "blips", -- Comando para iniciar a criação de blips.
     Permission = "Owner", -- Permissão para iniciar a criação de blips.
     Language = {
-        InitBlips = "Você iniciou a criação de blips.",
-        ExitBlips = "Você cancelou a criação de blips.",
-        AlredyBlips = "Você já está em uma criação de blips.",
-        TextLeave = "~w~Aperte ~b~F7~w~ para ~r~cancelar~w~ a criação de blips.~w~ ",
-        TextCreate = "~w~Aperte ~b~E~w~ para adicionar um ~r~blip~w~ no chão.~w~",
+        InitBlips = "Você iniciou a criação de blips.", -- Notify de inicio da criação de blips.
+        ExitBlips = "Você cancelou a criação de blips.", -- Notify de finalização da criação de blips.
+        AlredyBlips = "Você já está em uma criação de blips.", -- Notify para caso a pessoa já esteje em uma criação de blips.
+        TextLeave = "~w~Aperte ~b~F7~w~ para ~r~cancelar~w~ a criação de blips.~w~ ", -- Texto que aparecerá na tela para cancelar a criação de blips.
+        TextCreate = "~w~Aperte ~b~E~w~ para adicionar um ~r~blip~w~ no chão.~w~", -- Texto que aparecerá na tela para pegar a coordenada.
     },
 }
 
@@ -16,5 +16,5 @@ function UpdateTextBlips(coords,heading)
         [2] = "{ "..tD(coords.x)..", "..tD(coords.y)..", "..tD(coords.z)..", "..tD(heading).." },",
         [3] = "vector3("..tD(coords.x)..","..tD(coords.y)..","..tD(coords.z).."), heading = "..tD(heading)..",",
     }
-    return types[3] -- Numero do type acima
+    return types[3] -- Numero do type acima, basta colocar o numero desejado entre [] para estar mudando o type.
 end
